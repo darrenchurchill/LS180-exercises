656aa70996d5e4ddcc08e5145cf0704c218db1e1
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-08 12:09:31 -0700

    Add Easy 1 Ex 4

---
+++ 01_easy_1/04_select_data.sql
@@ -0,0 +1,17 @@
+/**
+ * Launch School Exercises
+ * LS180 - SQL Fundamentals
+ * Easy 1
+ *
+ * Select Data
+ *
+ * https://launchschool.com/exercises/35962964?track=javascript
+ */
+-- @block
+-- @conn animals
+-- @label view all birds
+SELECT
+  *
+FROM
+  birds
+;

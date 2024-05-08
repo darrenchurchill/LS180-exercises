eaeb8fdf05d491e3c7506c93a714250bb9d1284f
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-08 13:05:52 -0700

    Add Easy 1 Ex 9

---
+++ 01_easy_1/09_drop_table.sql
@@ -0,0 +1,14 @@
+/**
+ * Launch School Exercises
+ * LS180 - SQL Fundamentals
+ * Easy 1
+ *
+ * Drop Table
+ *
+ * https://launchschool.com/exercises/c5e3018b?track=javascript
+ */
+-- @block
+-- @conn animals
+-- @label drop birds table
+DROP TABLE IF EXISTS birds
+;

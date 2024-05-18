bb437d4bbc2b8c130070fcb08f506f5ddaf3feef
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-18 10:23:55 -0700

    Add DML Ex 1

---
+++ 04_DML/01_workshop.bash
@@ -0,0 +1,10 @@
+# Launch School Exercises
+# LS180 - SQL Fundamentals
+# DML (Data Manipulation Language)
+#
+# Set Up Database
+#
+# https://launchschool.com/exercises/f3a523f5?track=javascript
+#
+dropdb workshop
+createdb workshop
---
+++ 04_DML/01_workshop.sql
@@ -0,0 +1,32 @@
+/**
+ * Launch School Exercises
+ * LS180 - SQL Fundamentals
+ * DML (Data Manipulation Language)
+ *
+ * Set Up Database
+ *
+ * https://launchschool.com/exercises/f3a523f5?track=javascript
+ */
+-- @block
+-- @conn workshop
+-- @label create tables
+-- @label reset -- drop tables if exists
+DROP TABLE IF EXISTS devices,
+parts
+;
+
+-- @label create devices table
+CREATE TABLE devices (
+  id serial PRIMARY KEY,
+  name text NOT NULL,
+  created_at timestamp DEFAULT CURRENT_TIMESTAMP
+)
+;
+
+-- @label create parts table
+CREATE TABLE parts (
+  id serial PRIMARY KEY,
+  part_number int UNIQUE NOT NULL,
+  device_id int NOT NULL REFERENCES devices (id)
+)
+;

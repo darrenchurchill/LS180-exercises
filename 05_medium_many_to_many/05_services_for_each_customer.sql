17bf55226368736dbd418d2e85ab098e61347964
Darren Churchill <darrenmchurchill@gmail.com>
2024-05-20 14:18:21 -0700

    Add "Further Exploration" section

--- 05_medium_many_to_many/05_services_for_each_customer.sql
+++ 05_medium_many_to_many/05_services_for_each_customer.sql
@@ -20,3 +20,60 @@ FROM
 GROUP BY
   c.name
 ;
+
+/**
+ * Further Exploration
+ *
+ * Can you modify the statement above so the output looks like this?
+ *
+ *      name      |    description
+ * ---------------+--------------------
+ *  Chen Ke-Hua   | High Bandwidth
+ *                | Unix Hosting
+ *  Jim Pornot    | Dedicated Hosting
+ *                | Unix Hosting
+ *                | Bulk Email
+ *  Lynn Blake    | Whois Registration
+ *                | High Bandwidth
+ *                | Business Support
+ *                | DNS
+ *                | Unix Hosting
+ *  Nancy Monreal |
+ *  Pat Johnson   | Whois Registration
+ *                | DNS
+ *                | Unix Hosting
+ *  Scott Lakso   | DNS
+ *                | Dedicated Hosting
+ *                | Unix Hosting
+ * (17 rows)
+ *
+ */
+-- @label investigation -- show name, prev_name, and description
+SELECT
+  c.name,
+  lag(c.name) OVER (
+    ORDER BY
+      c.name
+  ) prev_name,
+  s.description
+FROM
+  customers c
+  LEFT OUTER JOIN customers_services cs ON c.id = cs.customer_id
+  LEFT OUTER JOIN services s ON cs.service_id = s.id
+;
+
+-- @label get services for each customer
+SELECT
+  nullif(
+    c.name,
+    lag(c.name) OVER (
+      ORDER BY
+        c.name
+    )
+  ) AS name,
+  s.description
+FROM
+  customers c
+  LEFT OUTER JOIN customers_services cs ON c.id = cs.customer_id
+  LEFT OUTER JOIN services s ON cs.service_id = s.id
+;
